b9d6e8621df3a48ee721fce3fec5fbb3db38360d
Thanatat Tamtan <acoshift@gmail.com>
2015-12-10 00:29:37 +0700

    move code and fixed #3

--- src/index.js
+++ src/index.js
@@ -50,168 +50,165 @@ interface Role {
   auth: any;
 }
 */
-MongoClient.connect(connectionUri, (err, db) => {
-  var app = express();
 
-  var nq = nepq();
+var app = express();
+var db;
 
-  function resp(err, r) {
-    r = (typeof r === 'undefined') ? null : r;
-    nq.response(r, err);
-  }
+MongoClient.connect(connectionUri, (err, database) => {
+  if (err) throw err;
 
-  function auth() {
-    let q = nq.request;
-    // TODO: auth logic
-    return true;
-  }
-
-  nq.on('auth', null, 'login', q => {
-    console.log(q);
-    let _db = q.namespace.shift();
-    db.db(_db).collection('nepq.user').findOne({
-      username: q.param.username,
-      password: q.param.password
-    }, (err, r) => {
-      if (err) return resp(500);
-      if (!r) return resp(401);
-
-      // create token
-      let token = uuid.v4();
-      db.db(_db).collection('nepq.token').insert({
-        token: token,
-        user: r._id,
-        created: Date.now(),
-        expire: Date.now() + (q.param.remember ? config.auth.expire.remember : config.auth.expire.time)
-      }, (err) => {
-        if (err) return resp(500);
-        nq.response({ token: token });
-      });
-    });
-  });
-
-  function extendTime(token) {
-    // TODO: add config.suth.expire.time to token
-  }
-
-  nq.on('auth', null, 'logout', q => {
-    let _db = q.namespace.shift();
-
-  });
-
-  nq.on('create', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    if (q.param instanceof Array) {
-      db.db(d).collection(c).insertMany(q.param, { w: 1 }, resp);
-      return;
-    }
-    db.db(d).collection(c).insertOne(q.param, { w: 1 }, resp);
-  });
-
-  nq.on('read', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    let limit = 0,
-        skip = 0;
-
-    if (typeof q.param.$limit !== 'undefined') {
-      limit = q.param.$limit;
-      delete q.param.$limit;
-    }
-
-    if (typeof q.param.$skip !== 'undefined') {
-      skip = q.param.$skip;
-      delete q.param.$skip;
-    }
-
-    db.db(d).collection(c).find(q.param).skip(skip).limit(limit).toArray(resp);
-  });
-
-  nq.on('$read', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    let skip = 0;
-
-    if (typeof q.param.$skip !== 'undefined') {
-      skip = q.param.$skip;
-      delete q.param.$skip;
-    }
-
-    db.db(d).collection(c).findOne(q.param, resp);
-  });
-
-  nq.on('update', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    if (!(q.param instanceof Array)) return resp(400);
-    if (q.param.length !== 2) return resp(400);
-
-    db.db(d).collection(c).updateMany(q.param.q, q.param.d, resp);
-  });
-
-  nq.on('$update', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    if (!(q.param instanceof Array)) return resp(400);
-    if (q.param.length !== 2) return resp(400);
-
-    db.db(d).collection(c).updateOne(q.param.q, q.param.d, resp);
-  });
-
-  nq.on('delete', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    db.db(d).collection(c).deleteMany(q.param, resp);
-  });
-
-  nq.on('$delete', null, null, q => {
-    if (!auth()) return;
-    let d = q.namespace.shift();
-    let c = q.namespace.join('.');
-
-    c = c ? `${c}.${q.name}` : q.name;
-
-    db.db(d).collection(c).deleteOne(q.param, resp);
-  });
-
-  nq.use(q => {
-    resp(404);
-  });
-
-  app.use(nq.bodyParser());
-
-  app.use((req, res) => {
-
-  });
+  db = database;
 
   let port = config.port || 8000;
 
   http.createServer(app).listen(port, () => {
-    console.log(`http server started at port ${port}`);
+    console.log(`Server listening on port ${port}`);
   });
+});
+
+var nq = nepq();
+
+function resp(err, r) {
+  if (err) {
+    nq.res.status(520).json(err);
+  } else {
+    nq.send(r);
+  }
+}
+
+function ns() {
+  let q = nq.request;
+  let d = q.namespace.shift();
+  let c = q.namespace.join('.');
+  c = c ? `${c}.${q.name}` : q.name;
+  return [ d, c ];
+}
+
+function auth() {
+  let q = nq.request;
+  // TODO: auth logic
+  return true;
+}
+
+nq.on('auth', null, 'login', q => {
+  console.log(q);
+  let _db = q.namespace.shift();
+  db.db(_db).collection('nepq.user').findOne({
+    username: q.param.username,
+    password: q.param.password
+  }, (err, r) => {
+    if (err) return resp(err);
+    if (!r) return resp(401);
+
+    // create token
+    let token = uuid.v4();
+    db.db(_db).collection('nepq.token').insert({
+      token: token,
+      user: r._id,
+      created: Date.now(),
+      expire: Date.now() + (q.param.remember ? config.auth.expire.remember : config.auth.expire.time)
+    }, (err) => {
+      if (err) return resp(500);
+      nq.response({ token: token });
+    });
+  });
+});
+
+function extendTime(token) {
+  // TODO: add config.suth.expire.time to token
+}
+
+nq.on('auth', null, 'logout', q => {
+  let _db = q.namespace.shift();
+
+});
+
+nq.on('create', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+  db.db(d).collection(c).insertMany(q.param, { w: 1 }, resp);
+});
+
+nq.on('$create', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+  db.db(d).collection(c).insertOne(q.param, { w: 1 }, resp);
+});
+
+nq.on('read', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+
+  let limit = 0,
+    skip = 0;
+
+  if (typeof q.param.$limit !== 'undefined') {
+    limit = q.param.$limit;
+    delete q.param.$limit;
+  }
+
+  if (typeof q.param.$skip !== 'undefined') {
+    skip = q.param.$skip;
+    delete q.param.$skip;
+  }
+
+  db.db(d).collection(c).find(q.param).skip(skip).limit(limit).toArray(resp);
+});
+
+nq.on('$read', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+
+  let skip = 0;
+
+  if (typeof q.param.$skip !== 'undefined') {
+    skip = q.param.$skip;
+    delete q.param.$skip;
+  }
+
+  db.db(d).collection(c).findOne(q.param, resp);
+});
+
+nq.on('update', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+
+  if (!(q.param instanceof Array)) return resp(400);
+  if (q.param.length !== 2) return resp(400);
+
+  db.db(d).collection(c).updateMany(q.param.q, q.param.d, resp);
+});
+
+nq.on('$update', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+
+  if (!(q.param instanceof Array)) return resp(400);
+  if (q.param.length !== 2) return resp(400);
+
+  db.db(d).collection(c).updateOne(q.param.q, q.param.d, resp);
+});
+
+nq.on('delete', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+
+  db.db(d).collection(c).deleteMany(q.param, resp);
+});
+
+nq.on('$delete', null, null, q => {
+  if (!auth()) return;
+  let [ d, c ] = ns();
+
+  db.db(d).collection(c).deleteOne(q.param, resp);
+});
+
+nq.use(() => {
+  nq.res.sendStatus(405);
+});
+
+app.use(nq.bodyParser());
+
+app.use((req, res) => {
+  res.sendStatus(406);
 });
