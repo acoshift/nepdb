acda39b78145917aeef1ee47a593d1baee20fc6f
Thanatat Tamtan <acoshift@gmail.com>
2015-12-15 00:43:49 +0700

    add timestamp on update

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ function resp(req, res, q, err, r) {
 
 function calc(k, v) {
   switch (k) {
-    case '$$bcrypt':
+    case '$bcrypt':
       return bcrypt.hashSync(v, bcrypt.genSaltSync(10));
   }
   return null;
@@ -114,34 +114,30 @@ function calc(k, v) {
 
 function preprocess(q) {
   _.forOwn(q, (v, k, a) => {
-    if (k.startsWith('$$')) {
+    if (k.startsWith('$')) {
       _.forOwn(v, (_v, _k, _a) => {
-        a[_k] = calc(k, _v);
+        let p = calc(k, _v);
+        if (p !== null) a[_k] = p;
       });
       delete a[k];
     } else if (typeof v === 'object') {
       preprocess(v);
+    } else if (k === '_id') {
+      a[k] = objectId(v);
     }
   });
 }
 
-nq.parser.on('after', q => {
-  // TODO: log here
-  preprocess(q.params);
-});
+nq.parser.on('after', q => preprocess(q.params) );
 
-function now() {
-  return moment.utc().format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-}
-
-function log(db, ns, user, query, result) {
-  let d = {
-    db: db,
-    ns: ns,
-    user: user.user,
-    query: query,
-    time: now()
+function log(q, req, ...args) {
+  let user = decodeToken((authToken(req)));
+  let l = {
+    user: user ? user.user : null,
+    q: q
   };
+  db.db(user.ns).collection('log').insertOne(l);
+  args.pop()();
 }
 
 function authToken(req) {
@@ -157,9 +153,8 @@ function auth(req, res, next) {
   try {
     user = jwt.verify(token, config.secret);
     if (!user || !user.user || !user.ns) throw new Error();
-  } catch (e) { return reject(res); }
+  } catch (e) { return next(); }
 
-  let q = nq.request;
   // TODO: check method and db name with user's roles
   /*db.db(user.ns).collection('user').findOne({
     user: user.user
@@ -171,6 +166,15 @@ function auth(req, res, next) {
 
   req.user = user;
   next();
+}
+
+function decodeToken(token) {
+  let user = null;
+  try {
+    user = jwt.decode(token, config.secret);
+    if (!user || !user.user || !user.ns) throw new Error();
+  } catch(e) { return null; }
+  return user;
 }
 
 function login(ns, user, pwd, cb) {
@@ -193,6 +197,8 @@ function login(ns, user, pwd, cb) {
   });
 }
 
+nq.use(log);
+
 nq.on('login', null, (q, req, res) => {
   login(q.name, q.params.user, q.params.pwd, (r) => {
     if (!r) reject(res);
@@ -201,99 +207,91 @@ nq.on('login', null, (q, req, res) => {
 });
 
 nq.on('refresh', '', (q, req, res) => {
-  let token = authToken(req);
-  let user;
-  try {
-    user = jwt.decode(token, config.secret);
-    if (!user || !user.user || !user.ns) throw new Error();
-  } catch(e) { return reject(res); }
+  let user = decodeToken((authToken(req)));
+  if (!user) reject(res);
   login(user.ns, user.user, user.pwd, (r) => {
     res.json(q.response(r));
   });
 });
 
 nq.on('create', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    db.db(d).collection(c).insertMany(q.params, { w: 1 }, resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  if (!(q.params instanceof Array)) return error(res, 'NepDBError', 'Parameter must be an array of object');
+  let [ d, c ] = ns(q);
+  db.db(d).collection(c).insertMany(q.params, { w: 1 }, resp.bind(this, req, res, q));
 });
 
 nq.on('$create', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    db.db(d).collection(c).insertOne(q.params, { w: 1 }, resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  if (q.params instanceof Array) return error(res, 'NepDBError', 'Parameter must be an object');
+  let [ d, c ] = ns(q);
+  db.db(d).collection(c).insertOne(q.params, { w: 1 }, resp.bind(this, req, res, q));
 });
 
 nq.on('read', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
+  if (!req.user) return reject(res);
+  let [ d, c ] = ns(q);
 
-    let x = q.params;
-    let opt = {};
+  let x = q.params;
+  let opt = {};
 
-    if (q.params.length >= 2) {
-      x = q.params[0];
-      opt = q.params[1];
-    }
+  if (q.params.length >= 2) {
+    x = q.params[0];
+    opt = q.params[1];
+  }
 
-    opt = {
-      limit: opt.limit || 0,
-      skip: opt.skip || 0
-    };
+  opt = {
+    limit: opt.limit || 0,
+    skip: opt.skip || 0
+  };
 
-    db.db(d).collection(c).find(x).skip(opt.skip).limit(opt.limit).toArray(resp.bind(this, req, res, q));
-  });
+  db.db(d).collection(c).find(x).skip(opt.skip).limit(opt.limit).toArray(resp.bind(this, req, res, q));
 });
 
 nq.on('$read', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    db.db(d).collection(c).findOne(q.params, resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  let [ d, c ] = ns(q);
+  db.db(d).collection(c).findOne(q.params, resp.bind(this, req, res, q));
 });
 
 nq.on('update', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    if (!(q.params instanceof Array) || q.params.length !== 2) {
-      return error(res, 'NepQ', 'Bad Request');
-    }
-    db.db(d).collection(c).updateMany(q.params[0], q.params[1], resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  let [ d, c ] = ns(q);
+  if (!(q.params instanceof Array) || q.params.length !== 2) {
+    return error(res, 'NepQError', 'Parameter must be an array of 2 objects');
+  }
+  q.params[1].$currentDate = { updated: true };
+  db.db(d).collection(c).updateMany(q.params[0], q.params[1], resp.bind(this, req, res, q));
 });
 
 nq.on('$update', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    if (!(q.params instanceof Array) || q.params.length !== 2) {
-      return error(res, 'NepQ', 'Bad Request');
-    }
-    db.db(d).collection(c).updateOne(q.params[0], q.params[1], resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  if (!(q.params instanceof Array) || q.params.length !== 2) {
+    return error(res, 'NepDBError', 'Parameter must be an array of 2 objects');
+  }
+  let [ d, c ] = ns(q);
+  q.params[1].$currentDate = { updated: true };
+  db.db(d).collection(c).updateOne(q.params[0], q.params[1], resp.bind(this, req, res, q));
 });
 
 nq.on('delete', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    db.db(d).collection(c).deleteMany(q.params, { w: 1 }, resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  let [ d, c ] = ns(q);
+  db.db(d).collection(c).deleteMany(q.params, { w: 1 }, resp.bind(this, req, res, q));
 });
 
 nq.on('$delete', null, (q, req, res) => {
-  auth(req, res, () => {
-    let [ d, c ] = ns(q);
-    db.db(d).collection(c).deleteOne(q.params, { w: 1 }, resp.bind(this, req, res, q));
-  });
+  if (!req.user) return reject(res);
+  let [ d, c ] = ns(q);
+  db.db(d).collection(c).deleteOne(q.params, { w: 1 }, resp.bind(this, req, res, q));
 });
 
 nq.use((q, req, res) => {
-  error(res, 'NepDB', 'Not Implemented');
+  error(res, 'NepDBError', 'Not Implemented');
 });
 
 nq.error((req, res) => {
-  error(res, 'NepQ', 'Bad Request');
+  error(res, 'NepQError', 'Bad Request');
 });
 
 app.use((req, res, next) => {
@@ -304,8 +302,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(auth);
+
 app.use(nq.bodyParser());
 
 app.use((req, res) => {
-  res.json({ error: { name: 'NepDB', message: 'Bad Request' }});
+  res.json({ error: { name: 'NepDBError', message: 'Bad Request' }});
 });
