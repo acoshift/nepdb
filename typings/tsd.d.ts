1b352f0780c2581cf4ffddf6236746185411b6fd
Thanatat Tamtan <acoshift@gmail.com>
2015-12-19 09:51:03 +0700

    add typing to repo
    since we update some typing to newer version

---
+++ typings/tsd.d.ts
@@ -0,0 +1,9 @@
+
+/// <reference path="node/node.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="mongodb/mongodb.d.ts" />
+/// <reference path="bcryptjs/bcryptjs.d.ts" />
+/// <reference path="jsonwebtoken/jsonwebtoken.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
